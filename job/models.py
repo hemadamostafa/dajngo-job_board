3bb3c9de8bc25960dd09569afce18fa13e4bef3a
hemadamostafa <courses.0n.line.by.tata@gmail.com>
2020-08-10 12:21:27 -0700

    --- Auto-Git Commit ---

--- job/models.py
+++ job/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+'''
+    - html widet
+    - validation
+    - db size
+'''
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100)
